0e4a4a79377e0026e1d951506435f37970b1aabe
kunalkale98 <kunal,k2898@gmail.com>
2021-02-15 17:49:17 +0530

    Store to dict and determine the percentage

--- flipCoinCombo.sh
+++ flipCoinCombo.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -x
 #Welcome to flip coin combination problem
 
-coin=$((RANDOM%2))
 isHead=1
+n=0
+iteration=10
+delcare -A coins
+h=0
+t=0
+while [ $n -lt $iteration ]
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq $isHead ]
+   	then
+      	coins[$n]=H
+			((h++))
+	else
+   	coins[$n]=T
+		((t++))
+	fi
+	((n++))
+done
 
-if [ $coin -eq $isHead ]
-   then
-      echo "Heads won"
-else
-   echo "Tails won"
-fi
+pert1=$((($iteration-$h)*10))
+pert2=$((($iteration-$t)*10))
 
+echo ${coins[*]}
+echo "Percentage of heads "$pert1
+echo "Percentage of tails "$pert2
