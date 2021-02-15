4fcf740d4b3560e926c8d3c038594f41a04afbaa
kunalkale98 <kunal,k2898@gmail.com>
2021-02-15 17:23:39 +0530

    Added: Flip Coin once

--- flipCoinCombo.sh
+++ flipCoinCombo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 #Welcome to flip coin combination problem
 
+coin=$((RANDOM%2))
+isHead=1
+
+if [ $coin -eq $isHead ]
+   then
+      echo "Heads won"
+else
+   echo "Tails won"
+fi
 
